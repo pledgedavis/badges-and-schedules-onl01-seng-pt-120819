8dba3bb7297f5a771bc1ff563ad0165832ebe2fd
pledgedavis <pledgedavis14@gmail.com>
2019-12-19 18:31:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,7 +27,7 @@ end
     counter = 0 
     new_array = []
     while counter < attendees.length
-    new_array << "Hello, #{attendees[counter]} You'll be assigned to room !"
+    new_array << "Hello, #{attendees[counter]} You'll be assigned to room #{counter + 1}!"
     counter +=1
   end
   new_array
