9f3e3936bd549ffab24ea0fc1f2e18c7fac2c534
pledgedavis <pledgedavis14@gmail.com>
2019-12-19 17:21:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+# def square_array(array)
+#   my_array = []
+#   array.each do |n|
+#     my_array << n **2 
+#   end
+#   return my_array
+# end
+
+# square_array([1,2,3])
+
+def badge_maker(name)
+#   return "Hello, my name is #{name}."
+# end
+
+return badge_maker("Arel")
+ puts "Hello, my name is #{"Arel"}."
+end
+
